cd090629ef9af7dca673792fc5682a0548b178c7
Joseph Sakos <josephsakos@gmail.com>
2024-08-17 11:17:09 +0800

    feat: update example to work with smaller regularizer

--- scripts/julia/Example of a Hidden Matching Pennies Game.jl
+++ scripts/julia/Example of a Hidden Matching Pennies Game.jl
@@ -24,7 +24,7 @@ using HMGExamples.Schemes
 const zeros = HMGExamples.Initializations.zeros
 
 
-function representation_map(dims; init=uniform, bias=zeros)
+function representation_map(dims; init=uniform, bias=zeros, activator=celu)
   layers = Dense[]
   sizehint!(layers, 1 + length(dims))
   prev_dim, state = iterate(dims) # We assume `length(dims) > 0`.
@@ -32,7 +32,7 @@ function representation_map(dims; init=uniform, bias=zeros)
     next = iterate(dims, state)
     if isnothing(next) break end
     dim, state = next
-    push!(layers, Dense(prev_dim => dim, celu; init=init, bias=bias(dim)))
+    push!(layers, Dense(prev_dim => dim, activator; init=init, bias=bias(dim)))
     prev_dim = dim
   end
   push!(layers, Dense(prev_dim => 1, sigmoid; init=init, bias=bias(1)))
@@ -43,14 +43,20 @@ end
 Random.seed!(0)
 
 (m₁, m₂) = S = (1, 1)
-χ₁ = representation_map([m₁, 1]; init=uniform(; low=-1, high=1))
-χ₂ = representation_map([m₂, 1]; init=uniform(; low=-1, high=1))
+χ₁ = representation_map([m₁, 1]; 
+  init=uniform(; low=-1, high=1), 
+  activator=x -> celu(x, 10)
+)
+χ₂ = representation_map([m₂, 1]; 
+  init=uniform(; low=-1, high=1),
+  activator=x -> celu(x, 10)
+)
 
-hg = L₂(MatchingPenniesGame(), 5e-1; x₀=repeat(0.5, 2))
+hg = L₂(MatchingPenniesGame(), 4e-2; x₀=fill(0.5, 2))
 bg = hide(hg, (χ₁, χ₂), S)
 # θ₀ = uniform(m₁ + m₂; low=-1, high=1)
 θ₀ = @MVector [1.25, 2.25]
 s = PreconditioningScheme(bg, θ₀, 1e-2; abstol=0)
-θ = Iterators.take(s, 500) |> collect
+θ = Iterators.take(s, 40000) |> collect
 x = [[χ₁(θₜ[begin:m₁]); χ₂(θₜ[m₁ + 1:m₁ + m₂])] for θₜ ∈ θ]
 
